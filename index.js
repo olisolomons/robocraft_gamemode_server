b0bb0874493be7e2786a0f4cd0240ae6e1c7bdbe
Oli <oas@st-andrews.ac.uk>
2019-06-14 15:28:27 +0100

    Allow channels to enable and disable bot, post messages to all enabled channels

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const Discord = require("discord.js");
 const express = require('express');
 const http = require('http');
 const sqlite3 = require('sqlite3-promise');
+const bodyParser = require('body-parser');
 
 app = express();
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('No page, this is a discord bot');
 });
@@ -14,28 +18,41 @@ setInterval(() => {
 
 const client = new Discord.Client();
 
-client.on("ready", () => {
+client.on("ready", async () => {
     console.log("I am ready!");
 });
 
-client.on("message", (message) => {
-    if (message.content.startsWith("ping")) {
-        message.channel.send("pong!");
+client.on("message", async (message) => {
+    if (message.content.startsWith("!enable_robocraft_gamemode")) {
+        try {
+            await db.runAsync('INSERT INTO channels VALUES (?)', message.channel.id);
+            await message.channel.send('Enabled!');
+        } catch (err) {
+            await message.channel.send('It was already enabled!');
+        }
+        await message.channel.send(JSON.stringify(await db.allAsync('SELECT * FROM channels')));
+    }
+    if (message.content.startsWith("!disable_robocraft_gamemode")) {
+        await db.runAsync('DELETE FROM channels WHERE id=?', message.channel.id);
+        await message.channel.send('Disabled!');
+        await message.channel.send(JSON.stringify(await db.allAsync('SELECT * FROM channels')));
     }
 });
 
 const db = new sqlite3.Database('./.data/sqlite.db');
 
-async function init() {
-
-    await db.runAsync('create table test(c int)');
-    await db.runAsync('insert into test values (12)');
-    await db.runAsync('insert into test values (123)');
-    let res = await db.allAsync('select * from test');
-    console.log(res);
+async function broadcast(message) {
+    let channels = await db.allAsync('SELECT * FROM channels');
+    for (let channel_id of channels) {
+        let channel = client.channels.get(channel_id.id);
+        await channel.send(message);
+    }
 }
-init().catch(e => {
-    console.error(e);
-})
+
+app.post('/broadcast', async (req, res) => {
+    console.log(req.body.msg)
+    broadcast(req.body.msg);
+    res.end();
+});
 
 client.login(process.env.TOKEN);
