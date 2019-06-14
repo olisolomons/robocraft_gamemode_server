561dc4f7b4c4fee0dc17199df7dc3661f07e4593
Oli <oas@st-andrews.ac.uk>
2019-06-14 01:16:58 +0100

    sqlite test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const express = require('express');
 const http = require('http');
+const sqlite3 = require('sqlite3-promise');
 
 app = express();
 app.get('/', (req, res) => {
@@ -22,5 +23,19 @@ client.on("message", (message) => {
         message.channel.send("pong!");
     }
 });
+
+const db = new sqlite3.Database('./.data/sqlite.db');
+
+async function init() {
+
+    await db.runAsync('create table test(c int)');
+    await db.runAsync('insert into test values (12)');
+    await db.runAsync('insert into test values (123)');
+    let res = await db.allAsync('select * from test');
+    console.log(res);
+}
+init().catch(e => {
+    console.error(e);
+})
 
 client.login(process.env.TOKEN);
